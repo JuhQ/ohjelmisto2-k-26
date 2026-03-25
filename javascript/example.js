8e4f400811084deaabf3db2ef612b959f468d378
Juha Tauriainen <juha@bin.fi>
2026-03-25 15:15:52 +0200

    Add javascript module 1 examples

---
+++ javascript/example.js
@@ -0,0 +1,23 @@
+console.log("Tervehdys example.js tiedostosta")
+
+console.log(document.querySelector("h1"))
+
+
+let arvo1 = 1
+arvo2 = 500
+const arvo3 = 1000
+
+const arvo4 = "1"
+
+const arvo4_2 = '1'
+const arvo4_3 = `1`
+
+// Mitä on 1+"1"?
+
+console.log(arvo1)
+console.log(arvo2)
+console.log(arvo3)
+console.log(arvo4)
+
+
+
